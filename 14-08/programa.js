49065053de2bfcae1c92a590c48892c65401e95f
EstefiNy <125267817+EstefiNy@users.noreply.github.com>
2023-08-14 10:39:17 -0300

    Add files via upload

---
+++ 14-08/programa.js
@@ -0,0 +1,14 @@
+const formProduto =
+    document.getElementById("formProduto");
+const produtoLista =
+    document.getElementById("produtosLista");
+
+formProduto.
+    addEventListener('submit', addProduto);
+
+
+const produtod = [];
+
+function addProduto(event){
+
+}
